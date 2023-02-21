10f5eddc335a8d488b2a4036eb1dee49b63cbe55
abhrajitray77 <abhrajitray77@gmail.com>
2023-02-21 23:11:05 +0530

    logout button added

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,20 +1,26 @@
 'use client'
-import { signOut } from 'next-auth/react'
+
+import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
+import { signOut, useSession } from 'next-auth/react'
 import React from 'react'
 import NewChat from './NewChat'
 
 const SideBar = () => {
+  const { data:session } = useSession();
   return (
     <div className="p-2 flex flex-col h-screen">
         <div className="flex-1">
             <div className=''>
                 <NewChat />
-                <button onClick={()=> signOut()}
-                 className="text-white font-bold text-3xl">
-                  sign out
-                 </button>
+
+                 
             </div>
         </div>
+
+        <button className='border-gray-700 border chatRow'>
+          <ArrowLeftOnRectangleIcon className="h-6 w-6" />
+          <h1>Logout</h1>
+        </button>
     </div>
   )
 }
