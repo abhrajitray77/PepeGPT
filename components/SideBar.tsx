cf29c3e611a71769a4c5ca7619f045e9f215d3e4
abhrajitray77 <abhrajitray77@gmail.com>
2023-02-23 22:24:06 +0530

    added profile pic and Chatrow functionality

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,26 +1,45 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 
+import { db } from '@/firebase'
 import { ArrowLeftOnRectangleIcon } from '@heroicons/react/24/outline'
+import { collection, orderBy, query } from 'firebase/firestore'
+import { useCollection } from 'react-firebase-hooks/firestore'
 import { signOut, useSession } from 'next-auth/react'
 import React from 'react'
 import NewChat from './NewChat'
+import ChatRow from './ChatRow'
 
 const SideBar = () => {
   const { data:session } = useSession();
+  const [chats, loading, error] = useCollection(
+    session && query(
+      collection(db, "users", session?.user?.email!, "chats"),
+       orderBy("createdAt", "asc")
+  )
+  );
+  console.log(chats);
   return (
     <div className="p-2 flex flex-col h-screen">
         <div className="flex-1">
             <div className=''>
                 <NewChat />
 
-                 
+                 {chats?.docs.map((chat) => (
+                  <ChatRow key={chat.id} id={chat.id} />
+                ))}
             </div>
         </div>
 
-        <button className='border-gray-700 border chatRow'
-        onClick={()=> signOut()}>
+        <button className='border-gray-700 border chatRow justify-between'
+          onClick={()=> signOut()}>
           <ArrowLeftOnRectangleIcon className="h-6 w-6" />
           <h1>Logout</h1>
+          <img
+          src = {session?.user?.image!} 
+          alt = "profile pic"
+          className='h-8 w-8 rounded-full my-auto'
+          />
         </button>
     </div>
   )
