6ceeb68c3f0cd9d3b4d5ed44546d2db69faa1a47
abhrajitray77 <abhrajitray77@gmail.com>
2023-03-22 23:26:33 +0530

    Message component added

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,4 +1,10 @@
+"use client"
+import { db } from '@/firebase';
+import { collection, orderBy, query } from 'firebase/firestore';
+import { useSession } from 'next-auth/react';
 import React from 'react'
+import { useCollection } from 'react-firebase-hooks/firestore';
+import Message from './Message';
 
 //New chat page
 
@@ -7,8 +13,22 @@ type Props = {
 };
 
 const Chat = ({chatId}: Props) => {
+  const {data: session } = useSession();
+
+  //mapping our items from the collection
+  //double checking if the session exists
+  const [messages] = useCollection(session && query(
+    collection(db, "users", session?.user?.email!, "chats", chatId, "messages"),
+    orderBy("createTime", "asc")
+  ))
+
+
   return (
-    <div className='flex-1'>Chat</div>
+    <div className='flex-1'>
+      {messages?.docs.map((message) => (
+        <Message key = {message.id} message={message.data()} />
+      ))}
+    </div>
   );
 }
 
